8981549c16d0952bbd437e5e552941666ae8087d
DIM-MOR <mdemon1977@yandex.ru>
2025-12-20 17:12:05 +0300

    README.md changed requirments.txt

--- myozon/myozon/view.py
+++ myozon/myozon/view.py
@@ -4,3 +4,25 @@ def hello_page(request):
     return HttpResponse("<h1>Hello, world!</h1>")
 
 
+
+
+def main_page(request):
+    return HttpResponse("main page")
+
+
+
+
+def product(request, id=1, color="black"):
+    resp = f"Product id: {id}, color: {color}"
+    return HttpResponse(resp)
+
+
+
+def product_amount(request, id, amount):
+    resp = f"Product id: {id}, amount: {amount}"
+    return HttpResponse(resp)
+
+
+def category(request, lang):
+    resp = f"language: {lang}"
+    return HttpResponse(resp)
